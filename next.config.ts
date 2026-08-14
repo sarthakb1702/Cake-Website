ddd81fa9b7677742f8ba892f310e68dd0b6e829c
sarthakb1702 <sarthakbhandari0123@gmail.com>
2026-08-14 17:50:00 +0530

    updated auth domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/__/auth/:path*",
+        destination: "https://cake-website-c1463.firebaseapp.com/__/auth/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;

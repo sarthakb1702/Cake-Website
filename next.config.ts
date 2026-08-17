5b51d635571d917892b6ceb725b3d703e9db7672
sarthakb1702 <sarthakbhandari0123@gmail.com>
2026-08-17 20:39:10 +0530

    fix google login problem

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,24 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/__/auth/:path*",
         destination: "https://cake-website-c1463.firebaseapp.com/__/auth/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
       },
     ];
   },
